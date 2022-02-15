14c1b8d4bb78d77441ca927312f1febc92ca8d42
rivajuniorfr <rivajunior.rec@gmail.com>
2022-02-15 18:52:01 -0300

    Criado o primeiro exercicio, com soma da media de 5 numeros

---
+++ exercicio.js
@@ -0,0 +1,27 @@
+const input = require("readline-sync");
+
+const nome = input.question("Digite seu nome: ");
+var n1 =  Number (input.question("Digite nota 1: "));
+var n2 =  Number (input.question("Digite nota 2: "));
+var n3 =  Number (input.question("Digite nota 3: "));
+var n4 =  Number (input.question("Digite nota 4: "));
+var n5 =  Number (input.question("Digite nota 5: "));
+
+const media = parseFloat(n1+n2+n3+n4+n5)/5
+
+
+if ( media <= 3.5 ){
+
+    console.log( nome + ", sua media foi, " + media + " Você foi reprovado.")
+
+} else if (media >= 3.6 && media <= 6.9 ){
+
+    console.log( nome + ", sua media foi, " + media + " Você está em recuperação.")
+} else{
+    console.log( nome + ", sua media foi, " + media +  " Você está aprovado.")
+
+}
+   
+
+
+
